eb7d551a62571cf4ad9f07226d0d198496fdf7b2
William <20107153@mail.wit.ie>
2025-11-01 21:46:51 +0000

    removeFromFavourites was in the wrong folder

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -9,6 +9,8 @@ import { QueryClientProvider, QueryClient} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import SiteHeader from "./components/siteHeader";
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
+
 
 
 const queryClient = new QueryClient({
@@ -31,6 +33,7 @@ const App = () => {
             <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
             <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
             <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={ <Navigate to="/" /> } />
           </Routes>
